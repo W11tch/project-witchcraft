20ce88cba029c1fb2f2391fb23d02f900c72af76
W11tch <ngmalk@gmail.com>
2025-08-30 14:47:43 +0200

    Delete Assets directory

--- Assets/Scripts/Core/ResourceEvents.cs
+++
@@ -1,9 +0,0 @@
-// Located at: Assets/Scripts/Core/ResourceEvents.cs
-using ProjectWitchcraft.Core;
-
-//
-public struct ResourceChangedEvent
-{
-    public ResourceType ResourceType;
-    public int NewAmount;
-}
